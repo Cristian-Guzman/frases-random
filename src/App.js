650f86c1afd6ee8e69a3ef7c83b785dfcd707abd
Cristian Guzman <cristiangt1813@gmail.com>
2021-11-15 15:21:38 -0500

    Subiendo el proyecto frases aleatorias FCC

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import { Boton, Contenedor, Frase, Source } from './styles/App.style';
 
 function App() {
+  const [contador, setContador] = useState(0)
+  const aumentarContador = () =>{
+    setContador(contador + 1);
+    if (contador === 4) {
+      setContador(0);
+    }
+  }
+    const frases = [
+      {
+        quote: "Frankly, my dear, I don't give a damn.",
+        source: "Gone With The Wind - 1939"
+      },
+      {
+        quote: "Here's looking at you, kid.",
+        source: "Casablanca - 1942"
+      },
+      {
+        quote: "You're gonna need a bigger boat.",
+        source: "Jaws - 1975"
+      },
+      {
+        quote: "May the force be with you.",
+        source: "Star Wars - 1977"
+      },
+      {
+        quote: "Toto, I've a feeling we're not in Kansas anymore.",
+        source: "The Wizard of Oz - 1939"
+      }
+    ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+        <Contenedor>
+              <Frase>{frases[contador].quote}</Frase>
+              <Source>{frases[contador].source}</Source>
+              <Boton onClick={() => aumentarContador()}>New quote</Boton>
+        </Contenedor>      
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/styles/App.style.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Contenedor = styled.div`
+    width: 60%;
+    height: 60%;
+    max-width: 70rem;
+    margin: auto auto;
+`
+
+export const Frase = styled.h3`
+    font-size: 2rem;
+    color: #000;
+    margin: 2rem 0;
+`
+export const Source = styled.h4`
+    font-size: 1.6rem;
+    color: #000;
+    margin-top: 1rem;
+`
+
+export const Boton = styled.button`
+    padding: 2rem 3rem;
+    background-color: #a3f13c; 
+    color: #000;
+    cursor: pointer;
+`
